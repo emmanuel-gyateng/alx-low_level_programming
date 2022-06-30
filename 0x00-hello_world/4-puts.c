af622b9a8301f296f4135c209a8b6acd3fefc52c
Emmanuel Okyere Gyateng <emmanuel.gyateng@amalitech.org>
2022-06-30 10:27:00 -0700

    asdsad

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-/*Program that outputs Programming is like building a multilingual puzzle*/
+
+
+
+/**
+ *
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+ *
+ */
+
 int main(void)
 
 {
