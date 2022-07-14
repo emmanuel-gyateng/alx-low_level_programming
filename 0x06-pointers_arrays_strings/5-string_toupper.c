91df1abdbed8ec7b1e1d77f5939cf879781c63b0
Emmanuel Okyere Gyateng <106739597+emmanuel-gyateng@users.noreply.github.com>
2022-07-14 08:06:08 +0000

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,23 +1,21 @@
 #include "main.h"
 /**
- * _strcmp - compares two strings
- * @s1: first string.
- * @s2: second string.
- * Return: 0 if s1 and s2 are equals,
- * another number if not.
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: input string.
+ * Return: the pointer to dest.
  */
 
-int _strcmp(char *s1, char *s2)
+char *string_toupper(char *s)
 {
-	int i = 0, op = 0;
+	int count = 0;
 
-	while (op == 0)
+	while (*(s + count) != '\0')
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
 	}
 
-	return (op);
+	return (s);
 }
